1092b40f1d6739de2ef2bd792b024bb0343bef15
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-12-14 03:06:09 +0500

    server

--- chats/consumers.py
+++ chats/consumers.py
@@ -3,27 +3,22 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import User
 from asgiref.sync import sync_to_async
 from userEx.models import *
-
 class PrivateChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.sender = self.scope['user']
         self.receiver_username = self.scope['url_route']['kwargs']['receiver_username']
         self.receiver = await sync_to_async(User.objects.get)(username=self.receiver_username)
-
-        self.room_group_name = f'chat_{self.sender.id}_{self.receiver.id}'
-
+        self.room_group_name = f'chat_{min(self.sender.id, self.receiver.id)}_{max(self.sender.id, self.receiver.id)}'
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         await self.accept()
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
-
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data['message']
